bc9ff7b9edeea1354fbd06c6621fda0ebc41f3f3
Naga Harish Madhav <nagamadhav2004@gmail.com>
2024-06-14 22:10:08 +0530

    initial project

---
+++ ChatPrj/ChatPrj/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for ChatPrj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from ChatApp import routing
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatPrj.settings")
+
+django_asgi_app = get_asgi_application()
+
+application= ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket":URLRouter(
+        routing.websocket_urlpatterns
+    )
+})
+
